91a111aec4289ee91a4e459de1cb2599ee714f83
Quique00789 <a1122150145@utch.edu.mx>
2024-04-04 08:42:30 -0600

    Css
    Css

--- src/components/SteamAchievement.jsx
+++ src/components/SteamAchievement.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
+import './SteamAchivement.css'; // Ensure that the './SteamAchievement.css' file exists and is accessible in the specified path.
 const SteamAchievement = ({ name, achieved, icon }) => {
   return (
     <div>
       <img src={icon} alt={name} />
       <h3>{name}</h3>
-      <p>Desbloqueado: {achieved ? 'Sí' : 'No'}</p>
     </div>
   );
 };
